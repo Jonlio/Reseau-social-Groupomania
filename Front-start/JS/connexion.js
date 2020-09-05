31d925d2da8e0827d1e6f18086674598c8dde80e
Jo <jo-lion@hotmail.fr>
2020-09-05 10:37:59 +0200

    expiration token

--- Front-start/JS/connexion.js
+++
@@ -1,6 +0,0 @@
-const connexion = document.getElementById('connexion');
- window.addEventListener('load', () => {
-    if( !sessionStorage.getItem('token')) {
-        return window.location.replace('index.html')
-    }
-})
